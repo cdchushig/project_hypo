a65d182450b541490378ff39f8143f137d346802
cdchushig <cdavid.chushig@gmail.com>
2023-05-04 13:27:42 +0200

    Sorted corr matrix for visualization

--- src/tab2img.py
+++ src/tab2img.py
@@ -9,6 +9,24 @@ from utils.loader import get_categorical_numerical_var_names, identify_type_feat
 import itertools
 from scipy.stats import pointbiserialr, spearmanr
 from phik import phik_from_array
+import scipy
+import scipy.cluster.hierarchy as sch
+
+
+def cluster_corr(corr_array, inplace=False):
+    pairwise_distances = sch.distance.pdist(corr_array)
+    linkage = sch.linkage(pairwise_distances, method='complete')
+    cluster_distance_threshold = pairwise_distances.max() / 2
+    idx_to_cluster_array = sch.fcluster(linkage, cluster_distance_threshold,
+                                        criterion='distance')
+    idx = np.argsort(idx_to_cluster_array)
+
+    if not inplace:
+        corr_array = corr_array.copy()
+
+    if isinstance(corr_array, pd.DataFrame):
+        return corr_array.iloc[idx, :].T.iloc[idx, :]
+    return corr_array[idx, :][:, idx]
 
 
 def plot_hists_real_noisy_var(df):
@@ -93,20 +111,32 @@ seed_value = 3232
 
 df_data = pd.read_csv(str(Path.joinpath(consts.PATH_PROJECT_DATA, 'others', 'bbdd_steno.csv')), sep=',')
 
-df_corr = compute_corr_mixed_dataset(df_data, 'label')
-plot_corr_mixed_dataset(df_corr, figsize=(8, 8))
-
 v_sex = df_data.loc[:, 'sex'].values.reshape(-1, 1)
+v_exercise = df_data.loc[:, 'exercise'].values.reshape(-1, 1)
 
 v_sex_noisy1 = NoiseGenerator().corrupt_input('masking', v_sex, fraction_noise=10, seed_value=seed_value)
 v_sex_noisy2 = NoiseGenerator().corrupt_input('masking', v_sex, fraction_noise=20, seed_value=seed_value)
 v_sex_noisy3 = NoiseGenerator().corrupt_input('masking', v_sex, fraction_noise=30, seed_value=seed_value)
 
+v_exercise_noisy1 = NoiseGenerator().corrupt_input('salt_and_pepper', v_exercise, fraction_noise=10, seed_value=seed_value)
+v_exercise_noisy2 = NoiseGenerator().corrupt_input('salt_and_pepper', v_exercise, fraction_noise=20, seed_value=seed_value)
+
 m_real_noisy = np.hstack((v_sex, v_sex_noisy1, v_sex_noisy2, v_sex_noisy3))
 df_real_noisy = pd.DataFrame(m_real_noisy)
-
-print(df_real_noisy)
-
+# print(df_real_noisy)
 # plot_hists_real_noisy_var(df_real_noisy)
+
+df_data['sexn1'] = v_sex_noisy1
+df_data['sexn2'] = v_sex_noisy2
+df_data['sexn3'] = v_sex_noisy3
+
+df_data['exercisen1'] = v_exercise_noisy1
+df_data['exercisen2'] = v_exercise_noisy2
+
+df_corr = compute_corr_mixed_dataset(df_data, 'label')
+plot_corr_mixed_dataset(df_corr, figsize=(10, 10))
+
+df_corr_sorted = cluster_corr(df_corr)
+plot_corr_mixed_dataset(df_corr_sorted, figsize=(10, 10))
 
 
